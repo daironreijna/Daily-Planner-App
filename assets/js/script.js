a5022bd3b6d8af5ef4d7673ad2f9bee9cdab501c
Dairon Reijna <dairon.reijna@gmail.com>
2023-09-19 12:52:04 +0100

    html structure
    worked on adding divs for time, description and save button.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-// create a variable to display current day in HTML
-let today = dayjs().$d;
+// create a variable to display current day and time in HTML
+let today = dayjs().format('dddd DD MMMM YYYY');
+let time = dayjs().format('hh:mm a x')
 
-$('#currentDay').text(today);
-
-console.log(today)
+$('#currentDay').text(`${today}`);
+$('#currentTime').text(`${time}`)
